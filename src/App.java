eecb619e58de3e836aac1abdea464daaf117f180
melihyilmaz <firat.yilmaz@ceng.deu.edu.tr>
2023-10-14 23:35:36 +0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!\n"); // \n is a new line
+        System.out.println("I love Java!");
+
+        /*
+        * This is a multi-line comment
+        */
+    }
+}
